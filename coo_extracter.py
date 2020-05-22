948c2bf9262da8b58841f3d39077f46a630cbb78
Rafey Iqbal Rahman <59226057+RafeyIqbalRahman@users.noreply.github.com>
2020-05-22 21:54:57 +0500

    Removed unnecessary dependencies

--- coo_extracter.py
+++ coo_extracter.py
@@ -1,6 +1,5 @@
 import pickle
 from sklearn import linear_model
-from sklearn import datasets, linear_model
 from sklearn.metrics import mean_squared_error, r2_score
 import matplotlib.pyplot as plt
 import os
